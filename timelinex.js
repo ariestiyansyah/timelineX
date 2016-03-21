a1f3f13ed1f480ce5c857ec60536fbf1bb6503f9
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-03-21 16:01:25 +0700

    Create links and tasks mongodb and add gantt feature with user login

--- timelinex.js
+++ timelinex.js
@@ -1,23 +1,80 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+TasksCollection = new Mongo.Collection("tasks");
+LinksCollection = new Mongo.Collection("links");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+Router.configure({
+  layoutTemplate: 'main'
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+Router.route('/masuk', {
+  name: 'masuk',
+  Template: 'masuk'
+});
+
+Router.route('/tentang', {
+  name: 'tentang',
+  template: 'tentang'
+});
+
+Router.route('/', {
+  name: 'beranda',
+  template: 'beranda',
+  data: function() {},
+  onBeforeAction: function(){
+    var currentUser = Meteor.userId();
+    if(createUser){
+      this.next();
+    } else {
+      this.redirect('/masuk');
     }
-  });
-}
+  },
+  onAfterAction: function(){
+    var currentUser = Meteor.userId();
+    if(currentUser){
+      setTimeout(function()
+    {
+      var hariStyle = function(date){
+        var dateToStr = gantt.date.date_to_str("%D");
+        if (dateToStr(date) == "Sun" || dateToStr(date) == "Sat") {
+          return "weekend";
+        }
+        return "";
+      };
+      gantt.config.start_date = new Date(2016,2,1);
+      gantt.config.end_date = new Date(2016,9,1);
+      gantt.config.scale_height = 84;
+      gantt.config.scale_unit = "month";
+      gantt.config.date_scale = "%F, %Y";
+      gantt.config.grid_width = 300;
+      gantt.config.min_column_width = 20;
+      gantt.config.subscales = [
+        {unit: "week", step:1, date:"Week %W"},
+        {unit: "day", step:1, date:"%d", css:hariStyle}
+      ];
+      gantt.config.columns = [
+        {name: "text", label: "Tasks", width: '*', tree: true },
+        {name: "add", label:""}. width: 44 }
+      ];
+      gantt.init("gantt_here");
+      gantt.meteor({tasks: TasksCollection, links: LinksCollection});
+    }, 0);
+  }
+},
+});
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup(function() {
+    if (Meteor.user.find().count() == 0) {
+      Accounts.createUser({
+        email: '',
+        password: ''
+      });
+    }
+  });
+
+  Meteor.publish('tasks', function() {
+    return TasksCollection.find();
+  });
+  Meteor.publish('links', function(){
+    return LinksCollection.find();
   });
 }
